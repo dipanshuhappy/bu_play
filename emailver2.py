24d5c5bca76ebd7a80cc843851f0a62fd8570d6a
MonaTheDon <104318895+MonaTheDon@users.noreply.github.com>
2022-05-31 17:51:15 +0530

    Update emailver2.py

--- emailver2.py
+++ emailver2.py
@@ -10,8 +10,8 @@ mail=Mail(app)
 
 app.config["MAIL_SERVER"]='smtp.gmail.com'
 app.config["MAIL_PORT"]=587
-app.config["MAIL_USERNAME"]='tanisha0714@gmail.com'
-app.config['MAIL_PASSWORD']='mayGodblessme'                    #you have to give your password of gmail account
+app.config["MAIL_USERNAME"]='xyz@gmail.com'
+app.config['MAIL_PASSWORD']='***'                    #you have to give your password of gmail account
 app.config['MAIL_USE_TLS']=True
 app.config['MAIL_USE_SSL']=False
 mail=Mail(app)
